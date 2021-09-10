746cae533d4cf20e725d9a6b026b4f301ea1e82d
hqruan <63276983+hqruan@users.noreply.github.com>
2021-09-09 17:41:45 -0700

    Update V0909.scala

--- V0909.scala
+++ V0909.scala
@@ -39,7 +39,6 @@ where user_age is not null and user_age > 0 and rgst_time > DATE_SUB(CURRENT_DAT
 """)
 
 //更新逻辑: 解密
-<<<<<<< rhq0909
 //主表落库+是否失业信息+年龄
 //解密函数:
 val key_S019_BICDT_TPQR_PIM_PB01A_ED:String = getKey("JMX2011124IxilE","-1")
@@ -68,8 +67,6 @@ using (id_card)
 where user_age is not null and user_age > 0 and rgst_time > DATE_SUB(CURRENT_DATE(),180)
 --只看半年以内的
 """)
-=======
->>>>>>> master
 
 //关联流水表
 //08-19: 加入客户编号字段
